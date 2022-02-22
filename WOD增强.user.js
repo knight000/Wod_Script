cfad2882d06e8dbd0e315374ec12900aa0fe38af
knight000 <36959630+knight000@users.noreply.github.com>
2022-02-22 17:47:47 +0800

    new

---
+++ WOD增强.user.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name         Wod增强
+// @namespace    http://tampermonkey.net/
+// @version      2.6.1
+// @description  try to take over the world!
+// @author       knight000
+// @match        http*://*.world-of-dungeons.org/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=world-of-dungeons.org
+// @require      https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    // 页面加载完毕后执行脚本
+    window.onload = function () {
+        // 相关模块，注释掉来取消使用
+        AutoVote();
+        AutoReduce();
+        ReplaceLink();
+    };
+})();
+
+/**
+ * @function 自动投票模块
+ */
+function AutoVote() {
+    var VoteBanner = $(".vote.banner > a");
+    var VoteReward = $(".vote.reward");
+    // 如果有$(".vote.banner")就表明是投票页面进行操作
+    if (VoteBanner.length != 0) {
+        // 移除链接，这样就不会弹出窗口了
+        VoteBanner.removeAttr("href");
+        for (var i = 0; i < VoteBanner.length; i++) {
+            console.log("[Wod增强]正在检测投票链接" + i);
+            try {
+                // 把投票链接后的说明提取出来，以此判定是否已投票，正则把多余的"去除
+                var VoteSpan = VoteReward[i].getElementsByTagName("span")[0].innerHTML.replace(/"/g, "");
+            } catch {
+                continue;
+            };
+            // 如果是5或者3荣誉就进行点击
+            if (VoteSpan[0] == '5' || VoteSpan[0] == '3') {
+                VoteBanner[i].click();
+                break;
+            };
+        };
+        console.log("[Wod增强]全部投票完毕");
+    } else {
+        // 非投票页面检测未投票就打开投票窗口，如果默认设置拦截弹出窗口的话就打不开了
+        if ($("center:contains('点链接获5')").length != 0) {
+            window.open($("a[href^='/wod/spiel/rewards/vote.php?']:last").attr("href"));
+            console.log("[Wod增强]非投票页面，未投票，已跳转");
+        } else {
+            console.log("[Wod增强]非投票页面，已投票");
+        }
+    };
+};
+
+/**
+ * @function 自动减少地域探索时间模块
+ */
+function AutoReduce() {
+    var Reduce = $('[name="reduce_dungeon_time"]');
+    // 有这个按钮就按一下
+    if (Reduce.length != 0) {
+        Reduce[0].click();
+        console.log("[Wod增强]已自动减少地域时间");
+    } else {
+        console.log("[Wod增强]无需自动减少地域时间");
+    };
+};
+
+/**
+ * @function 链接替换为新窗口打开模块
+ */
+function ReplaceLink() {
+    // 团队说明的快速链接
+    $("div.gadget.groupmsg.lang-cn a[target!='_blank']").attr("target", "_blank");
+    // 物品链接
+    $("a.item_unique[target!='_blank']").attr("target", "_blank");
+    // 拍卖详情
+    $("a:contains('详情')[target!='_blank']").attr("target", "_blank");
+    // 英雄详情
+    $("a.hero_active,a.hero_inactive").attr("target", "_blank");
+    console.log("[Wod增强]链接替换为新窗口打开");
+};
---
+++ WOD显示物品详情.user.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name         Wod显示物品详情
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  try to take over the world!
+// @author       knight000
+// @match        http*://*.world-of-dungeons.org/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=world-of-dungeons.org
+// @require      https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
+// @grant        unsafeWindow
+// ==/UserScript==
+
+var DetailsDivNotHold;
+var OldItemLink;
+(function () {
+    'use strict';
+    // 页面加载完毕后执行脚本
+    window.onload = function () {
+        var ItemList = $("a.item_unique,a.item_usable,a.item_unusable,a.item_later_usable");
+        // 给物品链接添加属性
+        for (var i = 0; i < ItemList.length; i++) {
+            ItemList.eq(i).attr("onmouseover", 'window.ItemDetails_Show("' + ItemList.eq(i).attr("href") + '")');
+            ItemList.eq(i).attr("onmouseout", "ItemDetails_Hide()");
+        };
+        // 添加显示的Div
+        var ND = $("<div></div>");
+        ND.attr({
+            "id": "ItemDiv",
+            "onmouseover": "ItemDetails_Hold()",
+            "onmouseout": "ItemDetails_Hide()",
+            "overflow": "auto"
+        });
+        ND.css({
+            "padding": "15px",
+            "weight": "auto",
+            "height": "auto",
+            "position": "absolute",
+            "z-index": "999",
+            "border": "1px none",
+            "background": "#000000"
+        });
+        ND.hide();
+        $("body").append(ND);
+        DetailsDivNotHold = true;
+    };
+
+    /**
+     * @function 显示
+     */
+    unsafeWindow.ItemDetails_Show = function ItemDetails_Show(ItemLink) {
+        var ItemDiv = $("#ItemDiv");
+        // 判断是否持续
+        if (DetailsDivNotHold == true) {
+            DetailsDivNotHold = false;
+            if (OldItemLink != ItemLink) {
+                OldItemLink = ItemLink;
+                ItemDiv.html("");
+                // 获取物品内容
+                $.get(ItemLink, function (result) {
+                    var ItemDetils = $("#details", $(result));
+                    var ItemMore = $("#link", $(result));
+                    $("#ItemDiv").append(ItemDetils, ItemMore);
+                });
+                // 获取鼠标相对于文档的坐标并将div设置到那
+                var position = getMousePosition(event);
+                ItemDiv.css("left", position.x - 10);
+                ItemDiv.css("top", position.y - 100);
+                ItemDiv.show();
+            };
+        };
+    };
+
+    /**
+    * @function 隐藏
+    */
+    unsafeWindow.ItemDetails_Hide = function ItemDetails_Hide() {
+        DetailsDivNotHold = true;
+        setTimeout(function DivHide() {
+            if (DetailsDivNotHold == true) {
+                $("#ItemDiv").hide();
+            }
+        }, 500);
+    };
+
+    /**
+     * @function 保持
+     */
+    unsafeWindow.ItemDetails_Hold = function ItemDetails_Hold() {
+        DetailsDivNotHold = false;
+    };
+})();
+
+/**
+ * @function 获取鼠标相对于文档的坐标（考虑页面滚动）
+ * @description 来源https://www.hangge.com/blog/cache/detail_2261.html
+ * */
+function getMousePosition(event) {
+    var x = y = 0,
+        doc = document.documentElement,
+        body = document.body;
+    if (!event) event = window.event;
+    if (window.pageYoffset) {//pageYoffset是Netscape特有
+        x = window.pageXOffset;
+        y = window.pageYOffset;
+    } else {
+        x = (doc && doc.scrollLeft || body && body.scrollLeft || 0)
+            - (doc && doc.clientLeft || body && body.clientLeft || 0);
+        y = (doc && doc.scrollTop || body && body.scrollTop || 0)
+            - (doc && doc.clientTop || body && body.clientTop || 0);
+    }
+    x += event.clientX;
+    y += event.clientY;
+    return { 'x': x, 'y': y };
+}
